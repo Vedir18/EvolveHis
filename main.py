8b1d34000fca9c1456f3abccd1ac60d9304cd42b
Vedir18 <dziegieljohn@gmail.com>
2020-04-24 13:46:22 +0200

    argparse added

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 from optimization import Optimization
+import argparse
 
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description='''\
+    Evolutionary algorithm / Evolutionary algorithm with a trained heuristic
+    ------------------------------------------------------------------------''')
+
+#parser.add_argument('-th', '--train_heuristic',  action='store_true',  default=False,
+#                        help="trains a heuristic and adds it to he algorithm")
+#parser.add_argument('-f', '--function', action='store', type=int, default=0,
+#                        help="defines the fitness function:"
+#                             "0 - quadratic sum"
+#                             "1 - sergei function")
+#parser.add_argument('-sm', '--success_mark', action='store', type=float, default=0,
+#                        help="defines the margin of error"
+#                             "after which the algorithm decides"
+#                             "that the found solution is sufficient"
+#                             "and stops"
+#                             "must be between 0 and 100"
+#                             "at 0 the algorithm will run until the max number of generations")
+#parser.add_argument('-g', '--generations', action='store', type=int,
+#                        help='defines the max number of generations')
+parser.add_argument('-d', '--dimension', action='store', type=int, default=1, help='defines number of dimensions in the fitness function')
+parser.add_argument('-l', '--lambd', action='store', type=int, default=90, help='defines algorithms lambda')
+parser.add_argument('-mu', '--mu', action='store', type=int, default=200, help='defines algorithms mu')
+parser.add_argument('-il', '--initial_length', action='store', default=200, type=int, help='defines size of an initial population')
+args = parser.parse_args()
 
 def main():
 
     inits = {
         'arguments': None,
         'sigmas': None,
-        'dim': 1,
-        'lmbd': 90,
-        'mu': 200,
-        'initial_len': 200
+        'dim': args.dimension,
+        'lmbd': args.lambd,
+        'mu': args.mu,
+        'initial_len': args.initial_length
     }
     optimization = Optimization(inits)
     previous_best = None
